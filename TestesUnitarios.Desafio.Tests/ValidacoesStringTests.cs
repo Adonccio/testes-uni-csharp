fde134394077b37e89d9837ab26f4ec6576e92bd
Adonccio <gustavoadoncio@gmail.com>
2023-12-31 17:25:41 -0300

    desafio finalizado

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -12,8 +12,8 @@ public class ValidacoesStringTests
         //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
 
         // Arrange
-        var texto = "a";
-        var resultadoEsperado = 0;
+        var texto = "matrix";
+        var resultadoEsperado = 6;
 
         // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
@@ -26,8 +26,8 @@ public class ValidacoesStringTests
     public void DeveContemAPalavraQualquerNoTexto()
     {
         // Arrange
-        var texto = "Esse é um texto qualquer";
-        var textoProcurado = "qualquer";
+        var texto = "Zidane";
+        var textoProcurado = "z";
 
         //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
         // Act
@@ -43,14 +43,14 @@ public class ValidacoesStringTests
     {
         // Arrange
         var texto = "Esse é um texto qualquer";
-        var textoProcurado = "teste";
+        var textoProcurado = "z";
 
         // Act
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
         //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        Assert.False(false);
     }
 
     //TODO: Corrigir a anotação [Fact]
@@ -60,7 +60,7 @@ public class ValidacoesStringTests
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
-        var textoProcurado = "teste";
+        var textoProcurado = "procurado";
 
         // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
